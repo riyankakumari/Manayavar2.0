4b9d7209f8adf18118b9d5aa39032b8afcfc54da
riyankakumari <riyanka664@gmail.com>
2026-08-26 06:47:28 +0000

    Clean up imports in AboutScreen component
    Removed unused imports from about.tsx.

--- about.tsx
+++ about.tsx
@@ -1,5 +1,5 @@
 import { Text, View, StyleSheet } from "react-native";
-import { Link, Stack } from "expo-router";
+//import { Link, Stack } from "expo-router";
 
 export default function AboutScreen() {
     return (
